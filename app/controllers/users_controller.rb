5b8b18c5a5265ca71122b7708d653915a0f30cf8
YYsvg <rara10202001@gmail.com>
2024-06-16 16:55:58 +0900

    create_mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,61 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
   def mypage
-    @user = current_user
+    # 年、月の取得
+    @year = Date.today.year
+    @month = Date.today.month
+
+    # 月毎の収支
+    @monthly_incomes = (1..12).map do |month|
+      start_date = Date.new(@year, month, 1)
+      end_date = start_date.end_of_month
+      category_incomes = {}
+
+      IncomeCategory.all.each do |income_category|
+        category_income_price = current_user.incomes.where(created_at: start_date..end_date, income_category_id: income_category.id).sum(:price)
+        category_incomes[income_category.name] = category_income_price
+      end
+        {
+          month: start_date.strftime("%-m月"),
+          total_incomes: current_user.incomes.where(created_at: start_date..end_date).sum(:price),
+          category_incomes: category_incomes
+        }
+    end
+
+    @monthly_outcomes = (1..12).map do |month|
+      start_date = Date.new(@year, month, 1)
+      end_date = start_date.end_of_month
+      category_outcomes = {}
+
+      OutcomeCategory.all.each do |outcome_category|
+        category_outcome_price = current_user.outcomes.where(created_at: start_date..end_date, outcome_category_id: outcome_category.id).sum(:price)
+        category_outcomes[outcome_category.name] = category_outcome_price
+      end
+      {
+        month: start_date.strftime("%-m月"),
+        total_outcomes: current_user.outcomes.where(created_at: start_date..end_date).sum(:price),
+        category_outcomes: category_outcomes
+      }
+    end
+
+    # 収入の総計
+    @income_total = current_user.incomes.sum(:price)
+    @outcome_total = current_user.outcomes.sum(:price)
+    @balance = @income_total - @outcome_total
+
+
+    # 当月の収支
+    current_month_income = @monthly_incomes.find { |income| income[:month] == Date.current.strftime("%-m月") }
+    @current_month_income_value = current_month_income[:total_incomes]
+
+    current_month_outcome = @monthly_outcomes.find { |outcome| outcome[:month] == Date.current.strftime("%-m月") }
+    @current_month_outcome_value = current_month_outcome[:total_outcomes]
+
+    # 年間収支
+    @yearly_incomes_total = @monthly_incomes.sum { |income| income[:total_incomes] }
+    @yearly_outcomes_total = @monthly_outcomes.sum { |outcome| outcome[:total_outcomes] }
+    @yearly_balance = @yearly_incomes_total - @yearly_outcomes_total
   end
 
   def calendar
@@ -9,5 +64,14 @@ class UsersController < ApplicationController
     @outcomes = current_user.outcomes
   end
 
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.permit(:name)
+  end
 
 end
